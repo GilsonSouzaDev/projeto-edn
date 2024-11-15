c4bad46147e9fd277de2a97ba11241a62bf2cfba
GilsonSouzaDev <gilsonsouza.dev@gmail.com>
2024-11-15 15:01:49 -0300

    codigo de soma em python usando função

---
+++ exercicio-2/soma.py
@@ -0,0 +1,17 @@
+valor1 = 0
+valor2 = 0
+resultadoFinal = 0
+
+valor1 = int(input("Digite o primeiro valor: "))
+valor2 = int(input("Digite o segundo valor: "))
+
+def soma (a, b):
+    resultado = 0
+    resultado = a + b
+    return resultado
+
+resultadoFinal = soma(valor1, valor2)
+
+print(resultadoFinal)
+
+
