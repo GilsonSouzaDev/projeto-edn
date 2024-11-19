f022c94eaa748f9d55ff0978d943da8f019e1596
GilsonSouzaDev <gilsonsouza.dev@gmail.com>
2024-11-19 20:12:21 -0300

    adicionado comentarios ao projeto diferencaproduto

--- exercicio-2/diferencaproduto.py
+++ exercicio-2/diferencaproduto.py
@@ -1,20 +1,26 @@
+#Declarando inicializando variaveis de entrada
 A = 0
 B = 0
 C = 0
 D = 0
 
+# Declarando e inicializando variavel de saida
 diferenca = 0
 
+# Coletando valores de entrada do usuario
 A = int(input("Digite o primeiro valor: "))
 B = int(input("Digite o segundo valor: "))
 C = int(input("Digite o terceiro valor: "))
 D = int(input("Digite o quarto valor: "))
 
+# funcao para realizar o calculo da diferença
 def dif(A, B, C, D):
     dif = 0
     dif = (A * B) - (C * D)
     return dif
 
+# Chamando a funcao para realizar o calculo e atribuindo a variavel de saida
 diferenca = dif(A, B, C, D)
 
+# Imprimando a mensagem e valor de saida
 print(f"DIFERENCA = {diferenca} ")
