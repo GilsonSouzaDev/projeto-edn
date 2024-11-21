0e69f0ea926c22ba8d3396d2815d4f4ae8f3735e
GilsonSouzaDev <gilsonsouza.dev@gmail.com>
2024-11-21 19:26:55 -0300

    adicionando projeto intervalo a pasta exercicio-3

---
+++ exercicio-3/intervalo.py
@@ -0,0 +1,14 @@
+intervalo = 0
+
+intervalo = float(input("Digite um valor de intevalo: "))
+
+if (0 <= intervalo <= 25):
+    print("Intervalo entre [0,25]")
+if (25 <= intervalo <= 50):
+    print("Intervalo entre [25,50]")
+if (50 <= intervalo <= 75):
+    print("Intervalo entre [50,75]")
+if (75 <= intervalo <= 100):
+    print("Intervalo entre [75,100]")
+if ( 0 > intervalo < 100):
+    print("Fora de intervalo")
